15b729a9a68cdee51e0eadc5019bee48a385d13f
teycir <teycir@gmail.com>
2026-05-15 23:39:49 +0100

    fix: deploy static assets correctly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,12 @@ set -e
 
 npm run pages:build
 cp .open-next/worker.js .open-next/_worker.js
+cp -r .open-next/assets/* .open-next/
+cat > .open-next/_routes.json << 'ROUTES'
+{
+  "version": 1,
+  "include": ["/*"],
+  "exclude": ["/_next/static/*", "/publiceth.svg", "/schema.sql"]
+}
+ROUTES
 wrangler pages deploy .open-next
